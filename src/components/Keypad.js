67dadb6b3ef0d4c7aea3d46a8d0ecf80ffafdbcb
Georgia Burnside <burnsidegeorgia@gmail.com>
2022-05-18 10:40:19 -0400

    completed lab

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,14 @@
-// Code Keypad Component Here
+import React from "react";
+
+function Keypad() {
+  function handleChange() {
+    console.log("Entering password...");
+  }
+  return (
+    <form>
+      <input type="password" onChange={handleChange} />
+    </form>
+  );
+}
+
+export default Keypad;
